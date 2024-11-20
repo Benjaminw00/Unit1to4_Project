b143ea989fe3ef5445af4e922da8ba3d406a7900
benjaminw00 <benjaminweng13@gmail.com>
2024-11-20 13:21:11 -0500

    new stuff

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Weather day1 = new Weather("monday", 60, "rain");
+        Weather day2 = new Weather("tuesday", 50, "cloudy");
+        Weather day3 = new Weather("wednesday", 40, "clear");
+        Weather day4 = new Weather("thursday", 70, "cloudy");
+        Weather day5 = new Weather("friday", 20, "clear");
+        Weather day6 = new Weather("saturday", -2, "snow");
+        Weather day7 = new Weather("sunday", 40, "hail");
+        day7.printTomorrowInfo();
+
+
     }
 }
--- src/Weather.java
+++ src/Weather.java
@@ -1,30 +1,78 @@
-import java.https://api.openweathermap.org/data/3.0/onecall?lat=40.7128&lon=74.0060&exclude=alerts&appid=4c0a4ed90318d02598a0c04e89f1b691;
 
+import javax.sound.midi.SysexMessage;
+import java.util.Locale;
 
 public class Weather {
 
-    private int temp;
+    private double temp;
+    private String weekday;
+    private String weatherCondition;
 
-    public void weather(){
+
+    public Weather(String weekday, double temp, String weatherCondition){
+        this.temp = temp;
+        this.weekday = weekday;
+        this.weatherCondition = weatherCondition;
     }
 
-    public String getTemperature(String weather){
 
+    public double getTemperature(){
+        return temp;
     }
 
-    public String jacket(int temperature){
+
+
+    public double getAverageWeekTemp(String weather1, String weather2, String weather3, String weather4, String weather5, String weather6, String weather7){
+        double total = 0;
+        for (int i = 0; i <= 7; i++){
+            total = getTemperature();
+        }
+        return total;
+    }
+
+
+    public String jacket(){
         if (temp<40){
             return "A jacket is recommended for today.";
         }
-        return "No, you don't need a jacket today.";
+        return "You don't need a jacket today.";
     }
 
-    public String umbrella(boolean rain){
-        if (rain){
+
+    public String umbrella(){
+        if (weatherCondition.equals("rain")){
             return "Bring an umbrella today.";
         }
         return "No need for an umbrella today";
     }
 
 
+    public void printInfo(){
+        weekday = (weekday.substring(0, 1)).toUpperCase(Locale.ROOT) + weekday.substring(1);
+        System.out.println(weekday + "'s temperature is sitting at " + temp +" degrees.");
+        if (weatherCondition.equals("rain") || weatherCondition.equals("snow")){
+            System.out.println("The weather is looking " + weatherCondition +"y.");
+        }
+        else if (weatherCondition.equals("hail")){
+            System.out.println("It will hail soon.");
+        }
+        else{
+            System.out.println("The weather will be " + weatherCondition + ".");
+        }
+    }
+
+    public void printTomorrowInfo() {
+        weekday = (weekday.substring(0, 1)).toUpperCase(Locale.ROOT) + weekday.substring(1);
+        String origWeekday = weekday;
+        int i = 0;
+        String[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        while (weekday.equals(origWeekday)){
+            if (weekday.equals(week[i])) {
+                weekday = week[i + 1];
+            }
+            i++;
+        }
+
+        System.out.println(weekday + "'s temperature is sitting at " + temp + " degrees.");
+    }
 }
